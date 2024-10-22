730ee95c1dfadd1fb4c39fdfb39333febd8d1924
Per Lindgren <per.lindgren@ltu.se>
2024-10-22 09:14:47 +0200

    from traits

--- src/device.rs
+++ src/device.rs
@@ -1,18 +1,38 @@
 //
 use std::{fs, path::PathBuf};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Device {
     Keyboard(PathBuf),
     Mouse(PathBuf),
     GamePad(PathBuf),
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum DeviceType {
     Keyboard,
     Mouse,
     GamePad,
+}
+
+impl From<Device> for DeviceType {
+    fn from(device: Device) -> Self {
+        match device {
+            Device::Keyboard(_) => DeviceType::Keyboard,
+            Device::Mouse(_) => DeviceType::Mouse,
+            Device::GamePad(_) => DeviceType::GamePad,
+        }
+    }
+}
+
+impl From<DeviceType> for Device {
+    fn from(device_type: DeviceType) -> Self {
+        match device_type {
+            DeviceType::Keyboard => Device::Keyboard(PathBuf::new()),
+            DeviceType::Mouse => Device::Mouse(PathBuf::new()),
+            DeviceType::GamePad => Device::GamePad(PathBuf::new()),
+        }
+    }
 }
 
 #[derive(Debug, Default)]
